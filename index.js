bb249758df7d2d485c74c6411a097bdc7417468c
cyliim <brickmanbots@gmail.com>
2019-06-25 16:20:59 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const fs = require("fs")
+const config = require("./config.json")
 
+client.commands = new Discord.Collection;
+client.aliases = new Discord.Collection;
+fs.readdir("./commands/", (err, files) =>{
+if(err) console.error(err)
+let jsfile = files.filter(f => f.split(".").pop() === "js")
+if(jsfile.length <= 0) {
+   return console.error("Commands not found. Check command file.");
+}
+jsfile.forEach((f, i) =>{
+    let pull = require(`./commands/${f}`);
+    client.commands.set(pull.config.name, pull);
+    pull.config.aliases.forEach(alias =>{
+        client.aliases.set(alias, pull.config.name);
+    })})});
+
+client.on("ready", () => {
+    console.log(`${client.user.tag} is running!`)
+}) 
+
+    client.on("message", message => {
+    if(message.author.bot || message.channel.type === "dm" || !message.content.startsWith(config.prefix)) return 
+    let messageArray = message.content.split(" ")
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+    let commandfile = client.commands.get(cmd.slice(config.prefix.length)) || client.commands.get(client.aliases.get(cmd.slice(config.prefix.length)))
+    if(commandfile) commandfile.run(client, message, args)
+    })
+
+    client.login(config.token)
