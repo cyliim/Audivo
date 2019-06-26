2a8bcee5f5d15da922bac139186ed1a7e6d2df3b
cyliim <brickmanbots@gmail.com>
2019-06-26 13:37:51 +1200

    Update modlog.js

--- commands/modlog.js
+++ commands/modlog.js
@@ -3,6 +3,8 @@ const db = require("quick.db")
 module.exports.run = async (client, message, args) => {
 
 }
-module.exports.config => {
-
+module.exports.config = {
+ "name":"modlog",
+  "aliases": ["sett"],
+  "description":"Set the guild modlog"
 }
