6e4410d77f67a0b1b5cbf9fabf2de63009067067
cyliim <brickmanbots@gmail.com>
2019-06-26 16:07:51 +1200

    Update modlog.js

--- commands/modlog.js
+++ commands/modlog.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js")
+    const Discord = require("discord.js")
 const db = require("quick.db")
 module.exports.run = async (client, message, args) => {
 if(!args[0]) return message.channel.send("Provide a channel to set the modlog to")
@@ -9,9 +9,9 @@ message.channel.send("Provide a channel name **without** mentioning it.")
 } else {
  db.set(`guildModLog_${message.guild.id}`, found.id)
  message.channel.send(`The new modlog for ${message.guild.id} has been set to ${found}`)
-}
+}}
 module.exports.config = {
- "name":"modlog",
-  "aliases": ["sett"],
-  "description":"Set the guild modlog"
+ name:"modlog",
+aliases: ["sett"],
+description:"Set the guild modlog"
 }
