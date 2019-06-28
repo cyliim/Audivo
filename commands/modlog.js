eeea9d8366e3e193ee3066b10a4c65fdacf3c6e2
cyliim <brickmanbots@gmail.com>
2019-06-28 16:19:35 +1200

    Update modlog.js

--- commands/modlog.js
+++ commands/modlog.js
@@ -1,15 +1,13 @@
     const Discord = require("discord.js")
 const db = require("quick.db")
 module.exports.run = async (client, message, args) => {
+    if(!message.member.hasPermission("ADMINISTRATOR" || "MANAGE_CHANNELS" || "MANAGE_SERVER"))
 if(!args[0]) return message.channel.send("Provide a channel to set the modlog to")
-let channel = args[0]
-let found = message.guild.channels.find(ch => ch.name === channel)
-if(!found) {
-message.channel.send("Provide a channel name **without** mentioning it.")
-} else {
- await db.set(`guildModLog_${message.guild.id}`, found.id)
- message.channel.send(`The new modlog for ${message.guild.name} has been set to ${found}`)
-}}
+let modlog = message.guild.channels.find(ch=>ch.name == args[0]);
+if(modlog) return message.channel.send(`The new modlog is now ${modlog}`)
+.then(await db.set(`guildModLog_${message.guild.id}`, modlog.id))
+message.channel.send('There was no channel provided in your message. Rather than mentioning the channel, use the name.');
+}
 module.exports.config = {
 name:"modlog",
 aliases: ["sett"],
